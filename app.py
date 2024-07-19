ce2d22c0f4ad40b20625e327af20677e82392cf7
Felix platzer <felix.platzer@limendo.com>
2024-07-19 12:24:18 +0200

    added better way to store client data and added fields for changing some variables

--- app.py
+++ app.py
@@ -1,57 +1,56 @@
 from flask import Flask, request, render_template, redirect, url_for
 from generateMap import generateMap
-
+import json
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    clients = [
-        "Lorenzo Bianchi",
-        "Giorgia Rinaldi",
-        "Federico Colombo",
-        "Anna Moretti",
-        "Matilde Ferrara",
-        "Sergio De Luca",
-        "Giulia Neri",
-        "Alessandro Fontana",
-        "Martina Rossi",
-        "Valentina Parisi",
-        "Andrea Ricci",
-        "Silvia Marino",
-        "Alberto Greco",
-        "Elisa Barone",
-        "Michela De Santis",
-        "Dario Marino",
-        "Sara Fontana",
-        "Riccardo Morelli",
-        "Chiara Rizzi",
-        "Luca Bianchi",
-        "Maria Rossi",
-        "Elena Verdi",
-        "Paolo Giordano",
-        "Sofia De Angelis",
-        "Gianni Lombardi",
-    ]
+    with open("clients.json", "r") as f:
+        clients = json.load(f)
+
+    clients = list(clients.keys())
+    default_avg_meeting_time = 60
+    default_time_at_main_client = 120
+    default_max_driving_time = 10 
     if request.method == "POST":
         client1 = request.form.get("client1")
         client2 = request.form.get("client2")
+        avg_meeting_time = int(
+            request.form.get("avgMeetingTime", default_avg_meeting_time)
+        )
+        time_at_main_client = int(
+            request.form.get("timeAtMainClient", default_time_at_main_client)
+        )
+        max_driving_time = int(
+            request.form.get("maxDrivingTime", default_max_driving_time)
+        )
 
         if client1 and client2 and client1 != client2:
-            generateMap(client1, client2)
+            generateMap(client1, client2, avg_meeting_time, time_at_main_client, max_driving_time)
             return redirect(url_for("output"))
         else:
             return render_template(
                 "index.html",
                 clients=clients,
                 error="Please select two different clients.",
+                avgMeetingTime=default_avg_meeting_time,
+                timeAtMainClient=default_time_at_main_client,
+                maxDrivingTime=default_max_driving_time,
             )
 
-    return render_template("index.html", clients=clients, error=None)
+    return render_template(
+        "index.html",
+        clients=clients,
+        error=None,
+        avgMeetingTime=default_avg_meeting_time,
+        timeAtMainClient=default_time_at_main_client,
+        maxDrivingTime=default_max_driving_time,
+    )
 
 
 @app.route("/output")
-def output(): 
+def output():
     return render_template("output.html")
 
 
